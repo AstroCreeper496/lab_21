05c1d2e801b24975a3b087a7f23ec562dbedfefe
AstroCreeper496 <pluccoqlu.1927@gmail.com>
2026-03-06 14:20:13 +0700

    Update average.cpp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,20 @@
+#include<cmath>
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
 
+int main(int argc, char *argv[]){
+    if(argc == 1) cout << "Please input numbers to find average.";
+    else if(argc >= 1) {
+        double sum = 0;
+        cout << "---------------------------------\n";
+        for(int v = 0; v < argc; v++){
+            sum += atof(argv[v]);
+        }
+        double mu = sum / (argc - 1);
+        cout << "Average of "<< argc - 1 <<" numbers = " << mu << endl;
+        cout << "---------------------------------\n";
+    }
+    return 0;
+}
